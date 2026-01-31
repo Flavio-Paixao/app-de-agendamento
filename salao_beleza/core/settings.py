c06fb5f8b9f51aabe12bb0fd67e52f372f1d043f
FlavioMVP <flavio_paixao92@icloud.com>
2026-01-31 16:18:11 -0300

    Movi os templates para a pasta correta

--- salao_beleza/core/settings.py
+++ salao_beleza/core/settings.py
@@ -102,13 +102,10 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
